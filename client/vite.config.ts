30bb642f7ad6f34b135e5132074e328e5b22f7f5
ewerz <77568704+afrinict@users.noreply.github.com>
2025-06-06 20:36:01 +0100

    Update: Modified Vite config to use dynamic port allocation

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -14,12 +14,13 @@ export default defineConfig({
     },
   },
   server: {
-    port: 2020,
-    strictPort: true,
+    port: 0,
+    strictPort: false,
     proxy: {
       '/api': {
         target: 'http://localhost:5600',
         changeOrigin: true,
+        secure: false,
       },
     },
   },
@@ -28,23 +29,16 @@ export default defineConfig({
     assetsDir: 'assets',
     sourcemap: false,
     rollupOptions: {
-      external: ['@mui/material', '@emotion/react', '@emotion/styled'],
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
           mui: ['@mui/material', '@emotion/react', '@emotion/styled'],
-        },
-        globals: {
-          '@mui/material': 'MaterialUI',
-          '@emotion/react': 'emotionReact',
-          '@emotion/styled': 'emotionStyled'
         }
       },
     },
   },
   optimizeDeps: {
-    include: ['@mui/material', '@emotion/react', '@emotion/styled'],
-    exclude: ['@mui/material', '@emotion/react', '@emotion/styled']
+    include: ['@mui/material', '@emotion/react', '@emotion/styled']
   },
   base: '/',
 }); 
